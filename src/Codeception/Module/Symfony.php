bd2d1525ae72b5f1c15cf7700026b8da4e62b43f
Thomas Landauer <thomas@landauer.at>
2020-12-02 16:52:09 +0100

    Renaming function argument without abbreviation

--- src/Codeception/Module/Symfony.php
+++ src/Codeception/Module/Symfony.php
@@ -895,24 +895,24 @@ class Symfony extends Framework implements DoctrineProvider, PartedModule
      *
      * ```php
      * <?php
-     * $I->seeInSession('attrib');
-     * $I->seeInSession('attrib', 'value');
+     * $I->seeInSession('attribute');
+     * $I->seeInSession('attribute', 'value');
      * ```
      *
-     * @param string $attrib
+     * @param string $attribute
      * @param mixed|null $value
      * @return void
      */
-    public function seeInSession(string $attrib, $value = null)
+    public function seeInSession(string $attribute, $value = null)
     {
         $session = $this->grabService('session');
 
-        if (!$session->has($attrib)) {
-            $this->fail("No session attribute with name '$attrib'");
+        if (!$session->has($attribute)) {
+            $this->fail("No session attribute with name '$attribute'");
         }
 
         if (null !== $value) {
-            $this->assertEquals($value, $session->get($attrib));
+            $this->assertEquals($value, $session->get($attribute));
         }
     }
 
