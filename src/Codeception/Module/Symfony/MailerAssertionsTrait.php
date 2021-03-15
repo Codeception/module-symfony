60ee0dced89afd9a6fecfcabd9219dcf3f2bb600
Thomas Landauer <thomas@landauer.at>
2021-03-15 17:04:55 +0100

    Update MailerAssertionsTrait.php

--- src/Codeception/Module/Symfony/MailerAssertionsTrait.php
+++ src/Codeception/Module/Symfony/MailerAssertionsTrait.php
@@ -13,6 +13,8 @@ trait MailerAssertionsTrait
 {
     /**
      * Checks that no email was sent.
+     * The check is based on `\Symfony\Component\Mailer\EventListener\MessageLoggerListener`, which means:
+     * If your app performs a HTTP redirect, you need to suppress it using [stopFollowingRedirects()](https://codeception.com/docs/modules/Symfony#stopFollowingRedirects) first; otherwise this check will *always* pass.
      */
     public function dontSeeEmailIsSent(): void
     {
@@ -38,6 +40,8 @@ trait MailerAssertionsTrait
 
     /**
      * Returns the last sent email.
+     * The function is based on `\Symfony\Component\Mailer\EventListener\MessageLoggerListener`, which means:
+     * If your app performs a HTTP redirect after sending the email, you need to suppress it using [stopFollowingRedirects()](https://codeception.com/docs/modules/Symfony#stopFollowingRedirects) first.
      *
      * ```php
      * <?php
@@ -60,6 +64,8 @@ trait MailerAssertionsTrait
 
     /**
      * Returns an array of all sent emails.
+     * The function is based on `\Symfony\Component\Mailer\EventListener\MessageLoggerListener`, which means:
+     * If your app performs a HTTP redirect after sending the email, you need to suppress it using [stopFollowingRedirects()](https://codeception.com/docs/modules/Symfony#stopFollowingRedirects) first.
      *
      * ```php
      * <?php
