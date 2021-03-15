8f84906cd1726f7db4cda47c08f1972afd1c57dc
Thomas Landauer <thomas@landauer.at>
2021-03-15 13:51:16 +0100

    Explaining the stopFollowingRedirects() restriction for email
    See https://github.com/Codeception/module-symfony/pull/107#issuecomment-799069222

--- src/Codeception/Module/Symfony/MailerAssertionsTrait.php
+++ src/Codeception/Module/Symfony/MailerAssertionsTrait.php
@@ -20,10 +20,9 @@ trait MailerAssertionsTrait
     }
 
     /**
-     * Checks if the desired number of emails was sent.
-     * Asserts that 1 email was sent by default, specify the `expectedCount` parameter to modify it.
-     * The email is checked using Symfony message logger, which means:
-     * * If your app performs a redirect after sending the email, you need to suppress it using [stopFollowingRedirects](https://codeception.com/docs/modules/Symfony#stopFollowingRedirects).
+     * Checks if the given number of emails was sent (default `$expectedCount`: 1).
+     * The check is based on `\Symfony\Component\Mailer\EventListener\MessageLoggerListener`, which means:
+     * If your app performs a HTTP redirect after sending the email, you need to suppress it using [stopFollowingRedirects()](https://codeception.com/docs/modules/Symfony#stopFollowingRedirects) first.
      *
      * ```php
      * <?php
